fc9692110222603b3aa470d094be7a9d03e4ad52
xM4ddy <59680197+xM4ddy@users.noreply.github.com>
2025-02-23 20:14:13 -0500

    inital

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="filter"></div>
+      <div className="filter2"></div>
+      <img className="logo" src="https://davidblaine.com/wp-content/uploads/2017/04/netflix-logo.png"></img>
+      <img className="background" src="https://assets.nflxext.com/ffe/siteui/vlv3/0cf2c109-3af1-4a9d-87d7-aecfac5fe881/web/US-en-20250217-TRIFECTA-perspective_fc7ae276-709f-4b60-afc6-4d6ececed8fd_large.jpg"></img>
+    </>
+  );
+}
+
+export default App;
